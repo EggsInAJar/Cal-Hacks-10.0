559f4e22da073c56752a5e1090aafb4da08d2004
EggsInAJar <66280529+EggsInAJar@users.noreply.github.com>
2023-10-28 00:07:55 -0700

    ye

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,19 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, SignOutButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { useUser } from "@clerk/clerk-react";
+import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 
 function App() {
   const [count, setCount] = useState(0)
   const tasks = useQuery(api.tasks.get);
+  const { isLoading, isAuthenticated } = useConvexAuth();
+  const { user } = useUser();
 
   return (
     <>
      <div className="App">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
       <SignInButton mode="modal" />
+      <SignOutButton mode="modal" />
+      {isAuthenticated ? "Logged in" : "Logged out or still loading"}
     </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
