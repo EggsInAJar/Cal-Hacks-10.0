fbf1febe2830f47c266f1474878ce449c46b3aaa
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2023-10-28 04:12:24 -0700

    Integrated Google Places API and basic React example

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useQuery } from "convex/react";
+import { useAction } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { SignInButton, SignOutButton } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
@@ -10,16 +11,32 @@ import { useUser } from "@clerk/clerk-react";
 import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
 
 function App() {
-  const [count, setCount] = useState(0)
   const tasks = useQuery(api.tasks.get);
+  const findRestaurants = useAction(api.findRestaurants.findRestaurants);
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [count, setCount] = useState(0)
   const { isLoading, isAuthenticated } = useConvexAuth();
   const { user } = useUser();
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await findRestaurants({location: '40.748817,-73.985428', radius: 500});
+      setRestaurants(result);
+    }
+  
+    fetchData();
+  }, []);
 
   return (
     <>
      <div className="App">
       <SignInButton mode="modal" />
       <SignOutButton mode="modal" />
+
+      {restaurants.length > 0 && restaurants.map(restaurant => (<li key={restaurant.place_id}>{restaurant.name}</li>))}
+      
       {isAuthenticated ? "Logged in" : "Logged out or still loading"}
     </div>
       <div>
