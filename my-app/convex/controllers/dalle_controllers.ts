5551980931cf4bdd48847b91d11c1c1e679f0b8d
“Sreeram <sreeramranga11@berkeley.edu>
2023-10-28 20:51:40 -0700

    bruh

--- my-app/convex/controllers/dalle_controllers.ts
+++ my-app/convex/controllers/dalle_controllers.ts
@@ -4,27 +4,33 @@ import Configuration from "openai";
 import OpenAIApi from "openai"
 import fetch from "node-fetch";
 import { v } from "convex/values";
-
-
 export const createDallE = action({
   args: { prompt: v.string() },  // Assuming the prompt is a string
   handler: async (ctx, args) => {
     const prompt = args.prompt;
+
+    // Print the received prompt
+    console.log("Received prompt:", prompt);
 
     // Step 0: Configure client
     "use node";
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     });
+
+    // Print the API key to ensure correctness
+    console.log("API Key:", process.env.OPENAI_API_KEY);
     
     const openai = new OpenAIApi(configuration);
-
 
     // Step 1: Check the prompt with the Moderation API
     const modResponse = await openai.createModeration({ input: prompt });
     const modResult = modResponse.data.results[0];
     if (modResult.flagged) {
+      console.log("Prompt was flagged:", JSON.stringify(modResult.categories));
       throw new Error(`Your prompt was flagged: ${JSON.stringify(modResult.categories)}`);
+    } else {
+      console.log("Prompt passed moderation.");
     }
 
     // Step 2: Generate an image using Dall-E
@@ -34,17 +40,18 @@ export const createDallE = action({
       size: "256x256",
     });
     const dallEImageUrl = opanaiResponse.data.data[0]["url"];
+    console.log("Generated DALL·E Image URL:", dallEImageUrl);
 
     // Step 3: Download the image
     const imageResponse = await fetch(dallEImageUrl);
     const image = await imageResponse.blob();
+    console.log("Image fetched.");
 
     // Step 4: Store the image in Convex
     const storageId = await ctx.storage.store(image);
     const permanentImageUrl = await ctx.storage.getUrl(storageId);
+    console.log("Image stored with URL:", permanentImageUrl);
 
     return permanentImageUrl;
   },
 });
-
-
