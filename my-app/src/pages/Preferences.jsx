186c806260dc0dfd76650824a10986a10dc4a4c2
Zaim Khan <zk6mg@umsystem.edu>
2023-10-29 05:10:25 -0500

    Preferences route completed

--- my-app/src/pages/Preferences.jsx
+++ my-app/src/pages/Preferences.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './Preferences.css';
 
 function Preferences() {
+  const goBackToMainPage = () => {
+    <a href="/"></a>
+  }
+
   const [selectedCuisines, setSelectedCuisines] = useState([]);
   const [priceRange, setPriceRange] = useState({ min: '', max: '' });
   const [favoriteFoods, setFavoriteFoods] = useState('');
@@ -12,6 +16,18 @@ function Preferences() {
 
   const cuisines = ['Italian', 'Japanese', 'Mexican', 'Indian', 'Chinese', 'American'];
   const dietaryRestrictions = ['Vegan', 'Vegetarian', 'Halal', 'Nut Allergies', 'Gluten-Free', 'No Beef', 'No Pork'];
+
+  const removeFromFoodsList = (index) => {
+    const newFavoriteFoodsList = [...favoriteFoodsList];
+    newFavoriteFoodsList.splice(index, 1);
+    setFavoriteFoodsList(newFavoriteFoodsList);
+  }
+
+  const removeFromDislikedFoodsList = (index) => {
+    const newDislikedFoodsList = [...dislikedFoodsList];
+    newDislikedFoodsList.splice(index, 1);
+    setDislikedFoodsList(newDislikedFoodsList);
+  }
 
   const handleCuisineChange = (cuisine) => {
     if (selectedCuisines.includes(cuisine)) {
@@ -30,6 +46,7 @@ function Preferences() {
   };
 
 const handleFavoriteFoodsSubmit = (e) => {
+  console.log(e)
   e.preventDefault();
   if (favoriteFoods) {
     setFavoriteFoodsList([...favoriteFoodsList, favoriteFoods]);
@@ -59,8 +76,22 @@ const handleDislikedFoodsSubmit = (e) => {
 
   return (
     <div className="favorite-cuisine-form">
+      <button
+        onClick={() => goBackToMainPage}
+        style={{
+          position: 'absolute',
+          top: '10px',
+          left: '10px',
+          padding: '5px',
+          backgroundColor: '#f9f9f9',
+          color: 'black',
+          borderColor: 'black',
+        }}
+      >
+        Back to Main
+      </button>
       <h2>Your Preferences</h2>
-      <form onSubmit={handleSubmit}>
+      <div>
         <div className="form-group">
           <label>Favorite Cuisines:</label>
           {cuisines.map((cuisine) => (
@@ -72,6 +103,21 @@ const handleDislikedFoodsSubmit = (e) => {
                 onChange={() => handleCuisineChange(cuisine)}
               />
               <label htmlFor={`cuisine-${cuisine}`}>{cuisine}</label>
+            </div>
+          ))}
+        </div>
+
+        <div className="form-group">
+          <label>Dietary Restrictions:</label>
+          {dietaryRestrictions.map((restriction) => (
+            <div key={restriction} className="checkbox-group">
+              <input
+                type="checkbox"
+                id={`restriction-${restriction}`}
+                checked={selectedDietaryRestrictions.includes(restriction)}
+                onChange={() => handleDietaryRestrictionChange(restriction)}
+              />
+              <label htmlFor={`restriction-${restriction}`}>{restriction}</label>
             </div>
           ))}
         </div>
@@ -93,21 +139,6 @@ const handleDislikedFoodsSubmit = (e) => {
         </div>
 
         <div className="form-group">
-          <label>Dietary Restrictions:</label>
-          {dietaryRestrictions.map((restriction) => (
-            <div key={restriction} className="checkbox-group">
-              <input
-                type="checkbox"
-                id={`restriction-${restriction}`}
-                checked={selectedDietaryRestrictions.includes(restriction)}
-                onChange={() => handleDietaryRestrictionChange(restriction)}
-              />
-              <label htmlFor={`restriction-${restriction}`}>{restriction}</label>
-            </div>
-          ))}
-        </div>
-
-        <div className="form-group">
           <label>Favorite Foods:</label>
           <form onSubmit={handleFavoriteFoodsSubmit}>
             <input
@@ -119,7 +150,10 @@ const handleDislikedFoodsSubmit = (e) => {
           </form>
           <div>
             {favoriteFoodsList.map((food, index) => (
-              <div key={index}>{food}</div>
+              <button key={index} onClick={() => removeFromFoodsList(index)} 
+              style={{display:'inline', margin:'1px', backgroundColor: '#f9f9f9', color: 'black', padding: '4px', borderColor: 'black'}}>
+                {food}
+              </button>
             ))}
           </div>
         </div>
@@ -136,132 +170,18 @@ const handleDislikedFoodsSubmit = (e) => {
           </form>
           <div>
             {dislikedFoodsList.map((food, index) => (
-              <div key={index}>{food}</div>
+              <button key={index} onClick={() => removeFromDislikedFoodsList(index)} 
+              style={{display:'inline', margin:'1px', backgroundColor: '#f9f9f9', color: 'black', padding: '4px', borderColor: 'black'}}>
+                {food}
+              </button>
             ))}
           </div>
         </div>
 
-        <button type="submit">Submit</button>
-      </form>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 };
-
-  
-
-
-
-
-
-
-// const Preferences = () => {
-//   const [selectedCuisines, setSelectedCuisines] = useState([]);
-//   const [priceRange, setPriceRange] = useState({ min: '', max: '' });
-//   const [favoriteFoods, setFavoriteFoods] = useState('');
-//   const [selectedDietaryRestrictions, setSelectedDietaryRestrictions] = useState([]);
-//   const [dislikedFoods, setDislikedFoods] = useState('');
-
-//   const cuisines = ['Italian', 'Japanese', 'Mexican', 'Indian', 'Chinese', 'American'];
-//   const dietaryRestrictions = ['Vegan', 'Vegetarian', 'Halal', 'Nut Allergies', 'Gluten-Free', 'No Beef', 'No Pork'];
-
-//   const handleCuisineChange = (cuisine) => {
-//     if (selectedCuisines.includes(cuisine)) {
-//       setSelectedCuisines(selectedCuisines.filter(c => c !== cuisine));
-//     } else {
-//       setSelectedCuisines([...selectedCuisines, cuisine]);
-//     }
-//   };
-
-//   const handleDietaryRestrictionChange = (restriction) => {
-//     if (selectedDietaryRestrictions.includes(restriction)) {
-//       setSelectedDietaryRestrictions(selectedDietaryRestrictions.filter(r => r !== restriction));
-//     } else {
-//       setSelectedDietaryRestrictions([...selectedDietaryRestrictions, restriction]);
-//     }
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Process the form data here (e.g., send to server, log in console, etc.)
-//     console.log({
-//       selectedCuisines,
-//       priceRange,
-//       favoriteFoods,
-//       selectedDietaryRestrictions,
-//       dislikedFoods
-//     });
-//   };
-
-//   return (
-//     <div className="favorite-cuisine-form">
-//       <h2>Your Preferences</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div className="form-group">
-//           <label>Favorite Cuisines:</label>
-//           {cuisines.map((cuisine) => (
-//             <div key={cuisine} className="checkbox-group">
-//               <input
-//                 type="checkbox"
-//                 id={`cuisine-${cuisine}`}
-//                 checked={selectedCuisines.includes(cuisine)}
-//                 onChange={() => handleCuisineChange(cuisine)}
-//               />
-//               <label htmlFor={`cuisine-${cuisine}`}>{cuisine}</label>
-//             </div>
-//           ))}
-//         </div>
-
-//         <div className="form-group">
-//           <label>Price Range:</label>
-//           <input
-//             type="number"
-//             placeholder="Min"
-//             value={priceRange.min}
-//             onChange={(e) => setPriceRange({ ...priceRange, min: e.target.value })}
-//           />
-//           <input
-//             type="number"
-//             placeholder="Max"
-//             value={priceRange.max}
-//             onChange={(e) => setPriceRange({ ...priceRange, max: e.target.value })}
-//           />
-//         </div>
-
-//         <div className="form-group">
-//           <label>Favorite Foods:</label>
-//           <textarea
-//             value={favoriteFoods}
-//             onChange={(e) => setFavoriteFoods(e.target.value)}
-//           />
-//         </div>
-
-//         <div className="form-group">
-//           <label>Dietary Restrictions:</label>
-//           {dietaryRestrictions.map((restriction) => (
-//             <div key={restriction} className="checkbox-group">
-//               <input
-//                 type="checkbox"
-//                 id={`restriction-${restriction}`}
-//                 checked={selectedDietaryRestrictions.includes(restriction)}
-//                 onChange={() => handleDietaryRestrictionChange(restriction)}
-//               />
-//               <label htmlFor={`restriction-${restriction}`}>{restriction}</label>
-//             </div>
-//           ))}
-//         </div>
-
-//         <div className="form-group">
-//           <label>Foods You Dislike:</label>
-//           <textarea
-//             value={dislikedFoods}
-//             onChange={(e) => setDislikedFoods(e.target.value)}
-//           />
-//         </div>
-
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
 
 export default Preferences;
